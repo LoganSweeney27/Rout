b5ff4b43c865975b0421c26375a97c5ff17ff949
Austin Wilson <wilso982@purdue.edu>
2021-02-27 13:54:45 -0500

    Added buttons and styling
    Created a button component and added it to the header. Also changed and worded with the styling of the web page and its components.

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
+    <div className='container'>
       <Header /*title='Username'*//>
     </div>
   );
--- example/src/components/Header.js
+++ example/src/components/Header.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    //Function used to change use of clicking on buttons.
+    const onClick = (e) => {
+        console.log(e)
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1 /*style={headingStyle}*/>Hello {title} this is Rout</h1>
+            <Button text='Button' onClick={onClick} />
         </header>
     )
 }
