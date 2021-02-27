594eec4ff07fb104f12fcf19b878adf7213c91f6
Austin Wilson <wilso982@purdue.edu>
2021-02-27 15:25:32 -0500

    Addition of Google Maps API
    Created the Google Maps API Key and then used Google Maps API to create a simple show static location (hard coded as Purdue campus) with location marker.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 //import logo from './logo.svg';
 import Header from './components/Header'
+import Map from './components/Map' // import the map here
+
+const location = {
+  address: '610 Purdue Mall, West Lafayette, IN 47907',
+  lat: 40.4237,
+  lng: -86.9212,
+} // location for google map api
 
 function App() {
   return (
     <div className='container'>
       <Header /*title='Username'*//>
+      <Map location={location} zoomLevel={16} />
     </div>
   );
 }
