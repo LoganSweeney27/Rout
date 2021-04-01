5803df34e383a06809d5b07b4b53eed73539ce26
williamspwhite <williamspwhite@gmail.com>
2021-03-31 22:19:50 -0400

    Added elevation map and fleshed out clear function

--- example/src/components/pages/Home/NewMap.js
+++ example/src/components/pages/Home/NewMap.js
@@ -23,6 +23,7 @@ class NewMap extends Component {
     this.state ={
       mapIsReady:false,
       query:"",
+      chartIsReady:false,
       places:[],
       filtered:[],
       m:[],
@@ -32,7 +33,6 @@ class NewMap extends Component {
       my_map:null,
     }
 
-    this.updateQuery = this.updateQuery.bind(this);
     this.initMap = this.initMap.bind(this)
 
   }
@@ -54,17 +54,34 @@ class NewMap extends Component {
   loadGoogleMapScript(){
     const ApiKey = 'AIzaSyDekWG_GZBqJ3j0Kt9t-B0ayBcU9wLHlsk'
 
-    const script = window.document.createElement('script')
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`
-    script.async = true;
-    script.defer= true;
-    script.onerror = function(){window.alert("The Google Maps API failed to load data!")}
-    
+    const scriptMap = window.document.createElement('script')
+    scriptMap.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`
+    scriptMap.async = true;
+    scriptMap.defer= true;
+    scriptMap.onerror = function(){window.alert("The Google Maps API failed to load data!")}
+
+    const scriptChart = window.document.createElement('script')
+
+    scriptChart.src = 'https://www.gstatic.com/charts/loader.js'
+    // scriptChart.async = true;
+    // scriptChart.defer = true;
+    scriptChart.onerror = function(){window.alert("The Google Charts API failed to load data!")}
+
     //this is a callback to wait until the code has loaded
-    script.addEventListener('load', () =>{
+    scriptMap.addEventListener('load', () =>{
       this.setState({mapIsReady:true})
+      
+
     });
-    window.document.body.appendChild(script)
+    scriptChart.addEventListener('load', () => {
+      if (this.state.mapIsReady) {
+        window.google.charts.load('current', {packages: ['corechart']});
+        this.setState({chartIsReady:true});
+
+      }
+    });
+    window.document.body.appendChild(scriptMap)
+    window.document.body.appendChild(scriptChart);
 
   }
 
@@ -72,6 +89,7 @@ class NewMap extends Component {
    //once the script is uploaded to the window load up the map 
     
     if (this.state.my_map == null) {
+      
       this.initMap()
     }
   }
@@ -80,8 +98,10 @@ class NewMap extends Component {
 
   //create markers
   initMap(){
+    
+
     //if map is ready to load
-    if(this.state.mapIsReady){
+    if(this.state.mapIsReady && this.state.chartIsReady){
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.4259, lng: -86.9081},
       zoom: 13,
@@ -91,45 +111,26 @@ class NewMap extends Component {
     }
     // Create A Map use window so the browser can access it
     //const map = this.state.my_map;
-    const directionsService = new window.google.maps.DirectionsService();
-    const directionsRenderer = new window.google.maps.DirectionsRenderer({
-      draggable: true,
-      map,
-    }); 
+
     if (this.state.d_renderer == null) {
+      const directionsRenderer = new window.google.maps.DirectionsRenderer({
+        draggable: true,
+        map,
+      }); 
       this.setState({d_renderer : directionsRenderer});
     }
     if (this.state.d_service == null) {
+      const directionsService = new window.google.maps.DirectionsService();
+
       this.setState({d_service : directionsService});
     }
 
-          
-    
-  //create markers
-
-
-
-
-  var startPointListener = map.addListener("click", (event) => {
-    addMarker(event.latLng, map);
-    startPoint = event.latLng;
-    window.google.maps.event.removeListener(startPointListener);
-    // while (this.state.routeDistance == "") {
-    //   //console.log("waiting");
-    // }
-  });
+    listenforStart(map);
+  
   
   }//end of if statement
 
   }
-
-
-  updateQuery = (query) =>{
-    this.setState({
-      query:query
-    })
-    this.updateSearchedPlaces(query)
-}
 
 
 gm_authFailure(){
@@ -144,14 +145,14 @@ gm_authFailure(){
   return response;
 }
 clearMap = () => {
-  window.alert("clearing map");
+  //window.alert("clearing map");
   this.state.d_renderer.setDirections(null);
   this.state.d_renderer.setMap(null);
   deleteMarkers();
   startPoint = null;
   waypts = [];
   wayptOn = false;
-  //initStartListener(map);
+  listenforStart(this.state.my_map);
 }
 addData = (data, e) => {
   //alert(e);
@@ -171,6 +172,7 @@ addData = (data, e) => {
           <Input onPress={ (data, e) => this.addData(data, e) } onClear={this.clearMap}/>
           {/* <h1>{this.state.routeDistance}</h1> */}
           <main id="map" role="application"></main>
+          <div id="elevation_chart"></div>
       </div>
     )
   }
@@ -184,7 +186,7 @@ function addMarker(location, map) {
       position: location,
       map: map,
   });
-  //markers.push(marker);
+  markers.push(marker);
 }
 
 function setMapOnAll(map) {
@@ -218,11 +220,8 @@ function myCalculateAndDisplayRoute(
 //   }
   //add marker at a location a given distance away
   //addMarker( {lat: 40.4259, lng: -86.9081}, map);
-  new window.google.maps.Marker({
-      position: newCoordinatesLocation(start.lat(), start.lng(),
-          distance/2, 90),
-      map,
-  });
+  addMarker(newCoordinatesLocation(start.lat(), start.lng(), distance/2, 90), map);
+
   console.log("added marker");
   //add that location as a waypoint
   if (!wayptOn) {
@@ -282,6 +281,10 @@ function myCalculateAndDisplayRoute(
               } else {
                   window.alert("WAYPOINTS TOO FAR AWAY, CLEAR AND TRY AGAIN")
               }
+              const elevator = new window.google.maps.ElevationService();
+
+                    // Draw the path, using the Visualization API and the Elevation service.
+                    displayPathElevation(route.overview_path, elevator, map);
 
           } else {
               window.alert("Directions request failed due to " + status);
@@ -296,4 +299,67 @@ function newCoordinatesLocation(lat, lng, distance, direction) {
   lng = lng + (distance * Math.sin(direction) / Math.cos(lat) / 111111);
   let latlng = new window.google.maps.LatLng(lat, lng);
   return latlng;
+}
+
+function displayPathElevation(
+  path,
+  elevator,
+  map
+) {
+
+// Create a PathElevationRequest object using this array.
+// Ask for 256 samples along that path.
+// Initiate the path request.
+elevator.getElevationAlongPath(
+    {
+      path: path,
+      samples: 256,
+    },
+    plotElevation
+);
+}
+
+// Takes an array of ElevationResult objects, draws the path on the map
+// and plots the elevation profile on a Visualization API ColumnChart.
+function plotElevation(elevations, status) {
+const chartDiv = document.getElementById("elevation_chart");
+
+if (status !== "OK") {
+// Show the error code inside the chartDiv.
+chartDiv.innerHTML =
+      "Cannot show elevation: request failed because " + status;
+return;
+}
+// Create a new chart in the elevation_chart DIV.
+const chart = new window.google.visualization.ColumnChart(chartDiv);
+
+// Extract the data from which to populate the chart.
+// Because the samples are equidistant, the 'Sample'
+// column here does double duty as distance along the
+// X axis.
+const data = new window.google.visualization.DataTable();
+data.addColumn("string", "Sample");
+data.addColumn("number", "Elevation");
+
+for (let i = 0; i < elevations.length; i++) {
+data.addRow(["", elevations[i].elevation]);
+}
+
+// Draw the chart using the data within its DIV.
+chart.draw(data, {
+height: 150,
+legend: "none",
+// @ts-ignore TODO(jpoehnelt) update to newest visualization library
+titleY: "Elevation (m)",
+});
+}
+
+
+function listenforStart(map) {
+  var startPointListener = map.addListener("click", (event) => {
+    addMarker(event.latLng, map);
+    startPoint = event.latLng;
+    window.google.maps.event.removeListener(startPointListener);
+
+  });
 }
