1dcf7d8911826559a02bc85c9c48e14ff7d9c45b
Austin Wilson <wilso982@purdue.edu>
2021-04-10 14:15:01 -0400

    Finished basic styling of details component

--- example/src/components/pages/Home/Details.js
+++ example/src/components/pages/Home/Details.js
@@ -2,12 +2,35 @@ import React from 'react'
 
 import './Details.css'
 
+var streetView = require('../../assests/google street view.png');
+
 const Details = () => {
     return (
         <div className='details-container'>
-            <h1 className='details-text'>
-                Details for route
-            </h1>
+            <div className='details-info'>
+                <div className='details-row'>
+                    <h1 className='details-text'>
+                        Distance: N/A
+                    </h1>
+                    <h1 className='details-text'>
+                        Time: N/A
+                    </h1>
+                    <h1 className='details-text'>
+                        Pace: N/A
+                    </h1>
+                </div>
+                <div className='details-row'>
+                    <h1 className='details-text'>
+                        Calories Burned: N/A
+                    </h1>
+                    <h1 className='details-text'>
+                        Starting Location: N/A
+                    </h1>
+                </div>
+            </div>
+            <div className='details-pic'>
+                <img className='details-img' src={streetView.default}  alt='Street View'/>
+            </div>
         </div>
     )
 }
