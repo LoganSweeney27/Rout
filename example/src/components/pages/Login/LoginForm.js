45230cc317152db97829c18a6a49218bd05ed94d
Logan Sweeney <lsweene@purdue.edu>
2021-04-21 20:08:58 -0400

    Default profile pic

--- example/src/components/pages/Login/LoginForm.js
+++ example/src/components/pages/Login/LoginForm.js
@@ -12,7 +12,7 @@ class LoginForm extends React.Component {
       username: '',
       password: '',
       nickname: '',
-      profilePicture: '',
+      profilePicture: 'https://t4.ftcdn.net/jpg/04/10/43/77/360_F_410437733_hdq4Q3QOH9uwh0mcqAhRFzOKfrCR24Ta.jpg',
       forgotCode: '',
       buttonDisabled: false,
       formDisabled: false,
